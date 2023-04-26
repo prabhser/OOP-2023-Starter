85a4c4760f26cad7f1bb3744ba8e63167c7d95bb
Prabhjeet Bissora <C21412282@mytudublin.ie>
2023-04-26 16:17:07 +0100

    created follow and word classes

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,11 @@
+package ie.tudublin;
+
+public class Follow {
+
+    String word;
+    int count;
+    void Follow(String word, int count){
+
+        System.out.println(word + toString(count));
+    }
+}
---
+++ java/src/ie/tudublin/Word.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+public class Word {
+
+
+    ArrayList<Follow> Follow = new ArrayList<Follow>;
+    String word;
+
+    void Word(String word, int count){
+        for (Follow f:Follow){
+            System.out.println(f);
+        }
+    }
+}
